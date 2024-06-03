dc22281ed0416fb6c58d97709808348f9b47b6d9
ralfy <ralfy@RALFY>
2024-06-03 17:43:06 -0500

    agregando en el contexto lo que me faltaba

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Ap1_P1_RalfyGenao.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ap1_P1_RalfyGenao.DAL
 {
     public class Contexto : DbContext
     {
+        public Contexto(DbContextOptions<Contexto> options)
+               : base(options) { }
+
+        public DbSet<Articulos> Articulos { get; set; }
     }
 }
