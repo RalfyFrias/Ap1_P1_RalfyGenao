a4751fd0507e64ad7f18001f37fa20e2162fb072
ralfy <ralfy@RALFY>
2024-06-03 17:35:00 -0500

    Creando el modelo de Articulos

---
+++ Models/Articulos.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ap1_P1_RalfyGenao.Models
+{
+    public class Articulos
+    {
+        [Key]
+        public int ArticuloId { get; set; }
+
+        [Required]
+        public string Descripcion { get; set; }
+        [Required]
+        
+        public int Monto { get; set; }
+        [Required]
+
+        public int Precio { get; set; }
+        [Required]
+
+        public int Ganancia { get; set; }
+      
+
+    }
+
+}
+
+// ArticuloID,Descripcion,Costo,Ganancia,Precio 
