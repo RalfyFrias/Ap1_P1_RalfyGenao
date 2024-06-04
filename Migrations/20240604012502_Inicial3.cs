d1b3400d795f2eed722139961070da44f24548c4
ralfy <ralfy@RALFY>
2024-06-03 20:26:18 -0500

    creanod nueva migracion

---
+++ Migrations/20240604012502_Inicial3.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ap1_P1_RalfyGenao.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Ganacia",
+                table: "Articulos",
+                newName: "Ganancia");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Ganancia",
+                table: "Articulos",
+                newName: "Ganacia");
+        }
+    }
+}
