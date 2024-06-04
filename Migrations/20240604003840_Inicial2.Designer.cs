5aa24f76844e572ec82688e4734e939bc738122d
ralfy <ralfy@RALFY>
2024-06-03 20:14:57 -0500

    creando y actualizando la migracion

--- Migrations/20240604003840_Inicial2.Designer.cs
+++ Migrations/20240604003840_Inicial2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Ap1_P1_RalfyGenao.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ap1_P1_RalfyGenao.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20240604003840_Inicial2")]
+    partial class Inicial2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.5");
---
+++ Migrations/20240604003840_Inicial2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ap1_P1_RalfyGenao.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Ganancia",
+                table: "Articulos",
+                newName: "Ganacia");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Fecha",
+                table: "Articulos",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Fecha",
+                table: "Articulos");
+
+            migrationBuilder.RenameColumn(
+                name: "Ganacia",
+                table: "Articulos",
+                newName: "Ganancia");
+        }
+    }
+}
